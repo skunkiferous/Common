2b22d0f5da73f2341b16408c3f68c7c640456279
skunkiferous <s.diot@eurodata.de>
2013-02-23 17:31:25 +0100

    Added AsLong

---
+++ src/com/blockwithme/util/AsLong.java
@@ -0,0 +1,11 @@
+package com.blockwithme.util;
+
+/**
+ * <code>AsLong</code> can be implemented by objects that can be represented as a primitive long.
+ *
+ * @author monster
+ */
+public interface AsLong {
+	/** Returns the primitive long representation. */
+	long asLong();
+}
